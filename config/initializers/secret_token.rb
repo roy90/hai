4c3955c7ee657dd4c70ea94cb7925b994b38e2d4
unknown <roy@roy-PC.(none)>
2012-11-18 00:48:34 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hai::Application.config.secret_token = '45ec7f9da64317a4f43dad5226d67c6a59dde7813e16ddf8ac4055c651d14bc830538b319ffc0f4c48b0d1d97b5625e62393d9716eab7abc5a890824277c29a4'
